9bff1f1a22416f9d67dde505876b8ad5fb29f531
Mirza Zunair Baig <mirza.zunair@clarisync.comm>
2024-11-04 11:47:01 +0500

    fix

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -6,16 +6,16 @@ describe('Test Case 1', () => {
     cy.get('#password').type('United@123');
     cy.get('#btnLogin').click();
     cy.wait(1000); 
-    cy.get('#quantum_projects > .hexagone-box-body > .ng-star-inserted').click({ force: true });
+    cy.get('#projects > .hexagone-box-body > .ng-star-inserted').click({ force: true });
     cy.wait(10000);
-    cy.xpath('//*[@id="pipeline"]', { timeout: 10000 }).click({ force: true });
-    cy.get('#btnPipeLineCreateNew').click({ force: true });
-    cy.get('#btnCreateCultivation').click({ force: true });
+    cy.xpath('//*[@id="btn"]', { timeout: 10000 }).click({ force: true });
+    cy.get('#btnCreateNew').click({ force: true });
+    cy.get('#btnCreate').click({ force: true });
     cy.wait(1000);
-    //cy.get('.side-nav-link-ref.ng-star-inserted.active#pipeline').should('have.class', 'active');
+    cy.get('.side-nav-link-ref.ng-star-inserted.active#pipeline').should('have.class', 'active');
     cy.wait(1000);
-    cy.get('#selectCultivationSite > .ng-select-container').find('.ng-dropdown-panel .ng-option').contains('Kyndryl : Greenford IRS (GBGRNFRD)').click();
-      //cy.get('.ng-input input').type('SEARCH_QUERY');
+    cy.get('#selectSite > .ng-select-container').find('.ng-dropdown-panel .ng-option').contains('IRS').click();
+    cy.get('.ng-input input').type('SEARCH_QUERY');
 
   })
 })
